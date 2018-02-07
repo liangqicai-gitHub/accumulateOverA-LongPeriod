e9213291331e3f55a138241c098814cbcc13cdd7
liangqicai-gitHub <2468751795@qq.com>
2018-02-07 10:54:37 +0800

    initMyProject

---
+++ AccumulateOverALongPeriod/AccumulateOverALongPeriod/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AccumulateOverALongPeriod
+//
+//  Created by 梁齐才 on 2018/2/7.
+//  Copyright © 2018年 梁齐才. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
