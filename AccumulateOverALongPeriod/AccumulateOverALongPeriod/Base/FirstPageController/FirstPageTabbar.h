cb39616ccb4e9182d7e8386b36889993f73f56de
liangqicai-gitHub <2468751795@qq.com>
2018-02-08 18:41:50 +0800

    aaa

---
+++ AccumulateOverALongPeriod/AccumulateOverALongPeriod/Base/FirstPageController/FirstPageTabbar.h
@@ -0,0 +1,13 @@
+//
+//  FirstPageTabbar.h
+//  AccumulateOverALongPeriod
+//
+//  Created by 梁齐才 on 2018/2/8.
+//  Copyright © 2018年 梁齐才. All rights reserved.
+//
+
+#import "RDDIYBottomBar.h"
+
+@interface FirstPageTabbar : RDDIYBottomBar
+
+@end
---
+++ AccumulateOverALongPeriod/AccumulateOverALongPeriod/Base/FirstPageController/FirstPageTabbar.m
@@ -0,0 +1,26 @@
+//
+//  FirstPageTabbar.m
+//  AccumulateOverALongPeriod
+//
+//  Created by 梁齐才 on 2018/2/8.
+//  Copyright © 2018年 梁齐才. All rights reserved.
+//
+
+#import "FirstPageTabbar.h"
+
+@implementation FirstPageTabbar
+
+- (instancetype)init{
+    self = [super init];
+    if (self){
+        [self initVarsAndViews];
+    }
+    return self;
+}
+
+- (void)initVarsAndViews{
+    
+}
+
+
+@end
